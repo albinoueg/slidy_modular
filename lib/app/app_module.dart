cfbfabd42ce7c4a0a4dc9545ed1ecbae529ef48a
Albino Moreira <albino.sistemas@gmail.com>
2020-02-16 12:28:11 -0300

    Aula 03 Mudança do projeto para utilização de módulos. Criação do pacote do modulo Home e reorganização da estrutura do projeto.

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,23 +3,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:slidy_modular/app/app_controller.dart';
 import 'package:slidy_modular/app/app_widget.dart';
-import 'package:slidy_modular/app/shared/repositories/poke_repository.dart';
+import 'package:slidy_modular/app/modules/home/home_module.dart';
 import 'package:slidy_modular/app/shared/utils/constants.dart';
-import 'package:slidy_modular/pages/home_controller.dart';
-import 'package:slidy_modular/pages/home_page.dart';
 
 class AppModule extends MainModule{
   @override
   List<Bind> get binds => [
     Bind((i) => AppController()),
-    Bind((i) => HomeController(i.get<PokeRepository>())),
-    Bind((i) => PokeRepository(i.get<Dio>())),
     Bind((i) => Dio(BaseOptions(baseUrl: URL_BASE))),
   ];
 
   @override
   List<Router> get routers => [
-    Router('/', child: (_, args) => HomePage()),
+    ///Passa a responsabilidade para o HomeModule
+    Router('/', module: HomeModule()),
   ];
 
   @override
---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:slidy_modular/app/modules/home/home_controller.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final homeController = Modular.get<HomeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Observer(
+        builder: (BuildContext context){
+          ///Mensagem de erro.
+          if(homeController.pokemons.error != null){
+            return Center(
+              child: RaisedButton(
+                child: Text('Erro prescione novamente'),
+                onPressed: (){
+                  homeController.fetchPokemons();
+                },
+              ),
+            );
+          }
+          ///Load caso ainda esteja carregando.
+          if(homeController.pokemons.value == null){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var list = homeController.pokemons.value;
+
+          return ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index){
+              return ListTile(
+                title: Text(list[index].name),
+              );
+            },
+          );
+        },
+      ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            //Navigator.pushNamed(context, '/other/${homeController.text}');
+            Modular.to.pushNamed('/other');
+          },
+          ),
+      );
+  }
+}
